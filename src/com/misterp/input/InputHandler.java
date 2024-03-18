7460ef8e2c30ed966407dc97a6379d866f5b09b8
Nicolas <30669856+GiveWife@users.noreply.github.com>
2024-03-18 14:34:34 +0100

    string part & integer part & documentation

--- src/com/misterp/input/InputHandler.java
+++ src/com/misterp/input/InputHandler.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class InputHandler {
 
+    /**
+     * Will try parsing input to integer. Upon failing, it will return null.
+     * @return an {@link Integer} object or null value
+     */
     public static Integer readInteger() {
         Scanner scanner = new Scanner(System.in);
         Object input;
@@ -26,6 +30,10 @@ public class InputHandler {
         }
     }
 
+    /**
+     * Will return the given input in String format.
+     * @return string
+     */
     public static String readString() {
         Scanner scanner = new Scanner(System.in);
         Object input;
@@ -43,5 +51,24 @@ public class InputHandler {
             }
         }
     }
+
+    /**
+     * Will strip the given input of any numbers.
+     * @param input a string
+     * @return returns the string part
+     */
+    public static String getStringPart(String input) {
+        return input.replaceAll("[0-9]", "");
+    }
+
+    /**
+     * Will strip the given input of string
+     * @param input a string
+     * @return a string value with numbers only
+     */
+    public static String getNumberPart(String input) {
+        return input.replaceAll("[^0-9]", "");
+    }
+
 
 }
--- src/com/misterp/printers/ArrayPrinter.java
+++ src/com/misterp/printers/ArrayPrinter.java
@@ -1,11 +1,21 @@
 package com.misterp.printers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class ArrayPrinter {
     public static <T> String arrayToString(T[] arr) {
-        return "[" + Arrays.stream(arr)
+        return "[" + stream(arr)
+                .map(Object::toString)
+                .collect(Collectors.joining(", ")) + "]";
+    }
+
+    public static <T> String listToString(List<T> arr) {
+        return "[" + arr.stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(", ")) + "]";
     }
